c353e1326dc956afd4b73230ff2b93a2ab2d88ee
ARJUN P <arjunpchr@gmail.com>
2023-12-31 20:17:58 +0530

    first commit for typescript

---
+++ typeScript_01.ts
@@ -0,0 +1,13 @@
+//Infer Types(Implicit Types)
+
+// let userName = 'Arjun';
+// userName = 20;
+
+// Defining Types(Explicit Types)
+
+let userName : string = 'Arjun';
+let age : number = 2_3;
+let isDeveloper : boolean = true;
+let names : string[] = ['arjun', 'appu'];
+let numbers : number[] = [1,2,3,4]; 
+let emptyArray : [] = [];
